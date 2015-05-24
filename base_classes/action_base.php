b1bf5fdf7883779d8091334aa5b8b60723e27dd8
Phil Hopper <phillip_hopper@wycliffeassociates.org>
2015-05-24 10:00:00 -0400

    Add action base

---
+++ base_classes/action_base.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Name: plugin_base.php
+ * Description: A base class for the syntax plugins.
+ *
+ * Author: Phil Hopper
+ * Date:   2015-05-20
+ */
+
+// must be run within Dokuwiki
+if(!defined('DOKU_INC')) die();
+
+
+
+class Door43_Action_Plugin extends DokuWiki_Action_Plugin {
+
+
+    /**
+     * Strings that need translated are delimited by @ symbols. The text between the symbols is the key in lang.php.
+     * @param $html
+     * @return mixed
+     */
+    protected function translateHtml($html) {
+
+        /* @var $door43shared helper_plugin_door43shared */
+        global $door43shared;
+
+        // $door43shared is a global instance, and can be used by any of the door43 plugins
+        if (empty($door43shared)) {
+            $door43shared = plugin_load('helper', 'door43shared');
+        }
+
+        if (!$this->localised) $this->setupLocale();
+        return $door43shared->translateHtml($html, $this->lang);
+    }
+}
